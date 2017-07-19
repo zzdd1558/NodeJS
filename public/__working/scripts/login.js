d88db3c54a1c9d3ad5d7306f83c88d5e24e33ae0
Choi, Yun jin <zzdd1558@naver.com>
2017-07-19 18:43:19 +0900

    #kakao success , naver fail 진행중

--- public/__working/scripts/login.js
+++ public/__working/scripts/login.js
@@ -9,34 +9,43 @@ import '../img/login/kakao.png';
 import '../img/login/naver.png';
 
 $(document).ready(function(){
-
-
     //kakao Login 부분
     Kakao.init('0e34b26f5967894741aa1e4f97e1537b');
-    $("#login-btn-kakao").click(function(){
+
+    $("#login_btn_kakao").click(function(){
        console.log("kakao");
         loginWithKakao();
     })
 
-    $("#login-btn-naver").click(function(){
-       console.log("naver");
+    $("#naver_id_login").click(function(){
+        console.log("naver");
+
     })
 
+    $("#login_btn_facebook").click(function(){
+        console.log("facebook");
+    })
 
-
-    function loginWithKakao() {
-        // 로그인 창을 띄웁니다.
-        Kakao.Auth.login({
-            success: function (authObj) {
-                alert(JSON.stringify(authObj));
-            },
-            fail: function (err) {
-                alert(JSON.stringify(err));
-            }
-        });
-    };
-
-
-
+    $("#login_btn_google").click(function(){
+        console.log("google");
+    })
 })
 
+
+function loginWithKakao() {
+    // 로그인 창을 띄웁니다.
+    Kakao.Auth.login({
+        success: function (authObj) {
+            alert(JSON.stringify(authObj));
+        },
+        fail: function (err) {
+            alert(JSON.stringify(err));
+        }
+    });
+};
+
+
+
+function loginWithFacebook(){
+
+}
