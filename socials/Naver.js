8a2a1a81e97b399caa86e4e0ef2ea97a578d8059
jp <whdvlf2006@gmail.com>
2017-07-24 16:56:22 +0900

    update

---
+++ socials/Naver.js
@@ -0,0 +1,31 @@
+const HttpRequest = require('../utils/HttpRequest');
+
+const client_id = 'VxgHymo8VpJl3iyxveUB';
+const client_secret = '4gsd0DLvZp';
+const redirectURI = encodeURI("http://localhost:3000/member");
+
+class Naver {
+
+    static async getProfile(accessToken) {
+        let http = new HttpRequest();
+        let profileApiUrl = 'https://openapi.naver.com/v1/nid/me';
+        let header = {'Authorization': `Bearer ${accessToken}`};
+
+        http.setHeaders(header);
+
+        return await http.sendRequest(profileApiUrl, {}, 'GET');
+    }
+
+    static async getAccessToken(code, state) {
+        let http = new HttpRequest();
+        let url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+            + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+        let header = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret};
+
+        http.setHeaders(header);
+
+        return await http.sendRequest(url, {});
+    }
+}
+
+module.exports = Naver;
---
+++ utils/HttpRequest.js
@@ -0,0 +1,28 @@
+const requestPromise = require('request-promise');
+
+class HttpRequest {
+
+    constructor() {
+        this.options = {
+            method: '',
+            uri: '',
+            headers: {},
+            json: true
+        };
+    }
+
+    async sendRequest(url, fields = {}, method = 'POST') {
+        this.options.uri = url;
+        this.options.form = fields;
+        this.options.method = method;
+
+        return await requestPromise(this.options);
+    }
+
+    setHeaders(header) {
+        this.options.headers = header;
+    }
+
+}
+
+module.exports = HttpRequest;
