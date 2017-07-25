e78184a2f20fd8a6d89a8a0cd85cec9d355ad8ec
jp <whdvlf2006@gmail.com>
2017-07-25 10:49:53 +0900

    update SocialFactory

--- socials/SocialFactory.js
+++ socials/SocialFactory.js
@@ -1,3 +1,16 @@
-/**
- * Created by parkjp on 2017-07-24.
- */
+
+class SocialFactory {
+
+    constructor(socialType) {
+    }
+
+}
+
+const SocialType = {
+    NAVER : 'naver',
+    FACEBOOK : 'facebook',
+    GOOGLE : 'google',
+    KAKAO : 'kakao'
+};
+
+module.exports = SocialFactory;
